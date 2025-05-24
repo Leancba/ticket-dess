ee0b56a2d7abc76fca580af803018a088caade60
Leancba <leandro.milia96@gmail.com>
2025-05-24 10:25:56 -0300

    eslint disable

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['images.unsplash.com', 'www.todopass.com.ar', 'blogger.googleusercontent.com'],
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
